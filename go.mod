37c8f3b9cc14f95f203885f1209f5d1bddca7d61
MarcoValeri <marcovaleri@hotmail.it>
2024-01-10 17:18:36 +0000

    Setup right door for platformsh

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module devwithgo
 
 go 1.21
+
+require github.com/platformsh/gohelper v1.0.0 // indirect
--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"devwithgo/controllers"
 	"net/http"
+
+	psh "github.com/platformsh/gohelper"
 )
 
 func main() {
+	// PlatformSH
+	platformSH, err := psh.NewPlatformInfo()
+	if err != nil {
+		panic("Not in a Platform.sh environment")
+	}
+
 	// Controllers
 	controllers.Home()
 
-	http.ListenAndServe(":80", nil)
+	// Local env
+	// http.ListenAndServe(":80", nil)
+
+	// Platform SH env
+	http.ListenAndServe(":"+platformSH.Port, nil)
 }
