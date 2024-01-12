0c68f3493c5c5750ae6928b91964b5d17c4a09aa
MarcoValeri <marcovaleri@hotmail.it>
2024-01-12 16:59:01 +0000

    Add templates

--- controllers/homeController.go
+++ controllers/homeController.go
@@ -3,17 +3,22 @@ package controllers
 import (
 	"html/template"
 	"net/http"
+	"time"
 )
 
 type PageData struct {
-	PageTitle string
+	PageTitle   string
+	CurrentYear int
 }
 
 func Home() {
-	tmpl := template.Must(template.ParseFiles("./views/home.html"))
+	setCurrentYear := time.Now().Year()
+
+	tmpl := template.Must(template.ParseFiles("./views/home.html", "./views/templates/head.html", "./views/templates/header.html", "./views/templates/footer.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := PageData{
-			PageTitle: "Dev With GO",
+			PageTitle:   "Do it with GO",
+			CurrentYear: setCurrentYear,
 		}
 		tmpl.Execute(w, data)
 	})
