32348fb3769e055783e1304c35483867cd458b4b
MarcoValeri <marcovaleri@hotmail.it>
2024-01-28 18:18:12 +0000

    Improve login section

--- controllers/adminControllers/adminLogin.go
+++ controllers/adminControllers/adminLogin.go
@@ -4,18 +4,71 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"net/mail"
+	"strings"
+
+	"github.com/microcosm-cc/bluemonday"
 )
+
+type LoginValidation struct {
+	EmailValidation    string
+	PasswordValidation string
+}
 
 func AdminLogin() {
 	tmpl := template.Must(template.ParseFiles("./views/admin/login.html"))
 	http.HandleFunc("/admin/login", func(w http.ResponseWriter, r *http.Request) {
 
-		// FORM validation
-		email := r.FormValue("email")
-		password := r.FormValue("password")
-		fmt.Println("Email:", email)
-		fmt.Println("Password:", password)
+		setLoginValidation := LoginValidation{
+			EmailValidation:    "",
+			PasswordValidation: "",
+		}
 
-		tmpl.Execute(w, nil)
+		// FORM validation
+		getEmail := r.FormValue("email")
+		getPassword := r.FormValue("password")
+
+		// Email: check user input
+		// Space at the left and right position of the string
+		getEmail = strings.TrimSpace(getEmail)
+
+		// Avoid HTML injection
+		sanitizeHtml := bluemonday.StrictPolicy()
+		getEmail = sanitizeHtml.Sanitize(getEmail)
+
+		// Check if email format is right
+		_, err := mail.ParseAddress(getEmail)
+		if err != nil {
+			setLoginValidation.EmailValidation = "Error: email format is not valid"
+			fmt.Println(setLoginValidation.EmailValidation)
+		}
+
+		// Check email length, no less that 5 charactes, no longer than 40 characters
+		if len(getEmail) < 5 || len(getEmail) > 40 {
+			setLoginValidation.EmailValidation = "Error: email length must be greater than 5 characters and no greater than 20 characters"
+			fmt.Println(len(getEmail))
+		}
+
+		// Password: check user input
+		// Remove space to the left and right of the string
+		getPassword = strings.TrimSpace(getPassword)
+
+		// Avodi HTML injection
+		getPassword = sanitizeHtml.Sanitize(getPassword)
+
+		// Check password length, minimum 8 charactes, max 20
+		if len(getPassword) < 8 || len(getPassword) > 20 {
+			setLoginValidation.PasswordValidation = "Error: password must be minimum 8 characters but no longher than 20 characters"
+		}
+
+		// Form validation
+		if getEmail == "info@marcovaleri.net" && getPassword == "1234" {
+			http.Redirect(w, r, "http://localhost/admin/dashboard", http.StatusSeeOther)
+		} else {
+			setLoginValidation.EmailValidation = "Error: email and password are not valid"
+			setLoginValidation.PasswordValidation = "Error: email and password are not valid"
+		}
+
+		tmpl.Execute(w, setLoginValidation)
 	})
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module devwithgo
 
 go 1.21
 
-require github.com/platformsh/gohelper v1.0.0 // indirect
+require (
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.26 // indirect
+	github.com/platformsh/gohelper v1.0.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
