ec2eeafafd10b96a21a92b2cd70b954e740d6058
MarcoValeri <marcovaleri@hotmail.it>
2024-02-03 18:29:08 +0000

    Create dev env

--- controllers/adminControllers/adminLogin.go
+++ controllers/adminControllers/adminLogin.go
@@ -27,46 +27,49 @@ func AdminLogin() {
 		// FORM validation
 		getEmail := r.FormValue("email")
 		getPassword := r.FormValue("password")
+		getLogin := r.FormValue("login")
 
-		// Email: check user input
-		// Space at the left and right position of the string
-		getEmail = strings.TrimSpace(getEmail)
+		if len(getLogin) > 0 {
+			// Email: check user input
+			// Space at the left and right position of the string
+			getEmail = strings.TrimSpace(getEmail)
 
-		// Avoid HTML injection
-		sanitizeHtml := bluemonday.StrictPolicy()
-		getEmail = sanitizeHtml.Sanitize(getEmail)
+			// Avoid HTML injection
+			sanitizeHtml := bluemonday.StrictPolicy()
+			getEmail = sanitizeHtml.Sanitize(getEmail)
 
-		// Check if email format is right
-		_, err := mail.ParseAddress(getEmail)
-		if err != nil {
-			setLoginValidation.EmailValidation = "Error: email format is not valid"
-			fmt.Println(setLoginValidation.EmailValidation)
-		}
+			// Check if email format is right
+			_, err := mail.ParseAddress(getEmail)
+			if err != nil {
+				setLoginValidation.EmailValidation = "Error: email format is not valid"
+				fmt.Println(setLoginValidation.EmailValidation)
+			}
 
-		// Check email length, no less that 5 charactes, no longer than 40 characters
-		if len(getEmail) < 5 || len(getEmail) > 40 {
-			setLoginValidation.EmailValidation = "Error: email length must be greater than 5 characters and no greater than 20 characters"
-			fmt.Println(len(getEmail))
-		}
+			// Check email length, no less that 5 charactes, no longer than 40 characters
+			if len(getEmail) < 5 || len(getEmail) > 40 {
+				setLoginValidation.EmailValidation = "Error: email length must be greater than 5 characters and no greater than 20 characters"
+				fmt.Println(len(getEmail))
+			}
 
-		// Password: check user input
-		// Remove space to the left and right of the string
-		getPassword = strings.TrimSpace(getPassword)
+			// Password: check user input
+			// Remove space to the left and right of the string
+			getPassword = strings.TrimSpace(getPassword)
 
-		// Avodi HTML injection
-		getPassword = sanitizeHtml.Sanitize(getPassword)
+			// Avodi HTML injection
+			getPassword = sanitizeHtml.Sanitize(getPassword)
 
-		// Check password length, minimum 8 charactes, max 20
-		if len(getPassword) < 8 || len(getPassword) > 20 {
-			setLoginValidation.PasswordValidation = "Error: password must be minimum 8 characters but no longher than 20 characters"
-		}
+			// Check password length, minimum 8 charactes, max 20
+			if len(getPassword) < 8 || len(getPassword) > 20 {
+				setLoginValidation.PasswordValidation = "Error: password must be minimum 8 characters but no longher than 20 characters"
+			}
 
-		// Form validation
-		if getEmail == "info@marcovaleri.net" && getPassword == "1234" {
-			http.Redirect(w, r, "http://localhost/admin/dashboard", http.StatusSeeOther)
-		} else {
-			setLoginValidation.EmailValidation = "Error: email and password are not valid"
-			setLoginValidation.PasswordValidation = "Error: email and password are not valid"
+			// Form validation
+			if getEmail == "info@marcovaleri.net" && getPassword == "1234" {
+				http.Redirect(w, r, "http://localhost/admin/dashboard", http.StatusSeeOther)
+			} else {
+				setLoginValidation.EmailValidation = "Error: email and password are not valid"
+				setLoginValidation.PasswordValidation = "Error: email and password are not valid"
+			}
 		}
 
 		tmpl.Execute(w, setLoginValidation)
