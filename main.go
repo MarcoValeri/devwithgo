0e22acc70a9cfa95120372fb71412740216105e3
MarcoValeri <marcovaleri@hotmail.it>
2024-02-03 19:35:27 +0000

    Add Platformsh env

--- main.go
+++ main.go
@@ -3,15 +3,16 @@ package main
 import (
 	"devwithgo/controllers"
 	"net/http"
-	// psh "github.com/platformsh/gohelper"
+
+	psh "github.com/platformsh/gohelper"
 )
 
 func main() {
 	// PlatformSH
-	// platformSH, err := psh.NewPlatformInfo()
-	// if err != nil {
-	// 	panic("Not in a Platform.sh environment")
-	// }
+	platformSH, err := psh.NewPlatformInfo()
+	if err != nil {
+		panic("Not in a Platform.sh environment")
+	}
 
 	// Static files
 	fs := http.FileServer(http.Dir("./public"))
@@ -22,8 +23,8 @@ func main() {
 	controllers.AdminController()
 
 	// Local env
-	http.ListenAndServe(":80", nil)
+	// http.ListenAndServe(":80", nil)
 
 	// Platform SH env
-	// http.ListenAndServe(":"+platformSH.Port, nil)
+	http.ListenAndServe(":"+platformSH.Port, nil)
 }
