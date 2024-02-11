cf59bd481f3996e1954b821123666483a8551e90
MarcoValeri <marcovaleri@hotmail.it>
2024-02-11 11:14:57 +0000

    Add db connection

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import (
 	"devwithgo/controllers"
 	"devwithgo/util"
 	"net/http"
-	// psh "github.com/platformsh/gohelper"
+
+	psh "github.com/platformsh/gohelper"
 )
 
 func main() {
 	// PlatformSH
-	// platformSH, err := psh.NewPlatformInfo()
-	// if err != nil {
-	// 	panic("Not in a Platform.sh environment")
-	// }
+	platformSH, err := psh.NewPlatformInfo()
+	if err != nil {
+		panic("Not in a Platform.sh environment")
+	}
 
 	// Static files
 	fs := http.FileServer(http.Dir("./public"))
@@ -22,12 +23,15 @@ func main() {
 	controllers.Home()
 	controllers.AdminController()
 
-	// Database
-	util.DatabaseConnection()
+	// Database Local
+	// util.DatabaseLocalConnection()
+
+	// Database Platform sh
+	util.DatabaseConnectionPlatformSh()
 
 	// Local env
-	http.ListenAndServe(":80", nil)
+	// http.ListenAndServe(":80", nil)
 
 	// Platform SH env
-	// http.ListenAndServe(":"+platformSH.Port, nil)
+	http.ListenAndServe(":"+platformSH.Port, nil)
 }
--- util/database.go
+++ util/database.go
@@ -2,39 +2,69 @@ package util
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+
+	psh "github.com/platformsh/config-reader-go/v2"
+	sqldsn "github.com/platformsh/config-reader-go/v2/sqldsn"
 )
 
-var db *sql.DB
+var dbPlatformSh *sql.DB
+var dbLocal *sql.DB
 
-func DatabaseConnection() *sql.DB {
+func DatabaseConnectionPlatformSh() *sql.DB {
+	config, err := psh.NewRuntimeConfig()
+	if err != nil {
+		log.Fatal("Some error occured. Err: $s", err)
+	}
+
+	credentials, err := config.Credentials("database")
+	if err != nil {
+		log.Fatal("Some error occured. Err: $s", err)
+	}
+
+	formatted, err := sqldsn.FormattedCredentials(credentials)
+	if err != nil {
+		log.Fatal("Some error occured. Err: $s", err)
+	}
+
+	dbPlatformSh, err := sql.Open("mysql", formatted)
+	if err != nil {
+		log.Fatal("Some error occured. Err: $s", err)
+	}
+
+	return dbPlatformSh
+}
+
+func DatabaseLocalConnection() *sql.DB {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Some error occured. Err: $s", err)
+	}
+
 	dbCredentials := mysql.Config{
-		User:                 "root",
-		Passwd:               "",
-		Net:                  "tcp",
-		Addr:                 "localhost:3306",
-		DBName:               "devwithgo",
+		User:                 os.Getenv("DB_USER"),
+		Passwd:               os.Getenv("DB_USER_PW"),
+		Net:                  os.Getenv("DB_USER_NET"),
+		Addr:                 os.Getenv("DB_USER_ADDR"),
+		DBName:               os.Getenv("DB_NAME"),
 		AllowNativePasswords: true,
 	}
 
 	// Get a database handle
-	var err error
-	db, err = sql.Open("mysql", dbCredentials.FormatDSN())
+	dbLocal, err = sql.Open("mysql", dbCredentials.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Println("DB credential: TRUE")
 	}
 
-	pinErr := db.Ping()
+	pinErr := dbLocal.Ping()
 	if pinErr != nil {
 		log.Fatal(pinErr)
-	} else {
-		fmt.Println("DB Ping: TRUE")
 	}
 
-	return db
+	return dbLocal
 }
