d4266cd6671defdbd84cc174d411904e21af8d54
MarcoValeri <marcovaleri@hotmail.it>
2024-03-11 12:12:42 +0000

    Add admin menu for adding a new guide

--- controllers/adminControllers/adminGuide.go
+++ controllers/adminControllers/adminGuide.go
@@ -1,14 +1,22 @@
 package admincontrollers
 
 import (
+	"devwithgo/models"
+	"devwithgo/util"
 	"fmt"
 	"html/template"
 	"net/http"
 )
 
 type guideData struct {
-	PageTitle string
-	Guides    string
+	PageTitle        string
+	TitleError       string
+	DescriptionError string
+	UrlError         string
+	PublishedError   string
+	UpdatedError     string
+	ContentErorr     string
+	Guides           []models.Guide
 }
 
 func AdminGuides() {
@@ -20,9 +28,14 @@ func AdminGuides() {
 			fmt.Println("Error on session-authentication:", errSession)
 		}
 		if session.Values["user-admin-authentication"] == true {
-			// TODO: get and show all guides
+			guidesData, err := models.GuideShowGuides()
+			if err != nil {
+				fmt.Println("Error getting guidesData: %w", err)
+			}
+
 			data := guideData{
 				PageTitle: "Guides",
+				Guides:    guidesData,
 			}
 
 			tmpl.Execute(w, data)
@@ -48,8 +61,8 @@ func AdminGuideAdd() {
 			}
 
 			// Flag validation
-			// var areAdminGuideInputsValid [6]bool
-			// isFormSubmittionValid := false
+			var areAdminGuideInputsValid [6]bool
+			isFormSubmittionValid := false
 
 			// Get value from the form
 			getAdminGuideTitle := r.FormValue("guide-title")
@@ -60,15 +73,93 @@ func AdminGuideAdd() {
 			getAdminGuideContent := r.FormValue("guide-content")
 			getAdminGuideAdd := r.FormValue("guide-add")
 
-			fmt.Println(getAdminGuideTitle)
-			fmt.Println(getAdminGuideDescription)
-			fmt.Println(getAdminGuideUrl)
-			fmt.Println(getAdminGuidePublished)
-			fmt.Println(getAdminGuideUpdated)
-			fmt.Println(getAdminGuideContent)
-			fmt.Println(getAdminGuideAdd)
+			// Sanitize form inputs
+			getAdminGuideTitle = util.FormSanitizeStringInput(getAdminGuideTitle)
+			getAdminGuideDescription = util.FormSanitizeStringInput(getAdminGuideDescription)
+			getAdminGuideUrl = util.FormSanitizeStringInput(getAdminGuideUrl)
+			getAdminGuidePublished = util.FormSanitizeStringInput(getAdminGuidePublished)
+			getAdminGuideUpdated = util.FormSanitizeStringInput(getAdminGuideUpdated)
+			getAdminGuideAdd = util.FormSanitizeStringInput(getAdminGuideAdd)
 
-			// TODO: validate form and add the guide to the db
+			// Check if the form has been submitted
+			if getAdminGuideAdd == "Add new guide" {
+				// Title validation
+				if len(getAdminGuideTitle) > 0 && len(getAdminGuideTitle) < 60 {
+					data.TitleError = ""
+					areAdminGuideInputsValid[0] = true
+				} else {
+					data.TitleError = "Title should be between 1 to 60 charactes"
+					areAdminGuideInputsValid[0] = false
+				}
+
+				// Description validation
+				if len(getAdminGuideDescription) > 0 && len(getAdminGuideDescription) < 160 {
+					data.DescriptionError = ""
+					areAdminGuideInputsValid[1] = true
+				} else {
+					data.DescriptionError = "Description should be between 1 t0 160 charactes"
+					areAdminGuideInputsValid[1] = false
+				}
+
+				// Url validation
+				if len(getAdminGuideUrl) > 0 {
+					data.UrlError = ""
+					areAdminGuideInputsValid[2] = true
+				} else {
+					data.UrlError = "Add an URL"
+					areAdminGuideInputsValid[2] = false
+				}
+
+				// Published validation
+				if len(getAdminGuidePublished) > 0 {
+					data.PublishedError = ""
+					areAdminGuideInputsValid[3] = true
+				} else {
+					data.PublishedError = "Add a data"
+					areAdminGuideInputsValid[3] = false
+				}
+
+				// Updated validation
+				if len(getAdminGuideUpdated) > 0 {
+					data.PublishedError = ""
+					areAdminGuideInputsValid[4] = true
+				} else {
+					data.UpdatedError = "Add a data"
+					areAdminGuideInputsValid[4] = false
+				}
+
+				// Content validation
+				if len(getAdminGuideContent) > 0 {
+					data.ContentErorr = ""
+					areAdminGuideInputsValid[5] = true
+				} else {
+					data.UpdatedError = "Add a data"
+					areAdminGuideInputsValid[5] = false
+				}
+
+				for i := 0; i < len(areAdminGuideInputsValid); i++ {
+					isFormSubmittionValid = true
+					if !areAdminGuideInputsValid[i] {
+						isFormSubmittionValid = false
+						break
+					}
+				}
+
+				// Create a new guide if all inputs are valid
+				if isFormSubmittionValid {
+					createNewGuide := models.GuideNew(
+						1,
+						getAdminGuideTitle,
+						getAdminGuideDescription,
+						getAdminGuideUrl,
+						getAdminGuidePublished,
+						getAdminGuideUpdated,
+						getAdminGuideContent,
+					)
+					models.GuideAddNewToDB(createNewGuide)
+					http.Redirect(w, r, "/admin/guides", http.StatusSeeOther)
+				}
+			}
 
 			tmpl.Execute(w, data)
 		} else {
--- models/guideModel.go
+++ models/guideModel.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"devwithgo/database"
+	"fmt"
+)
+
 type Guide struct {
 	Id          int
 	Title       string
@@ -10,7 +15,62 @@ type Guide struct {
 	Content     string
 }
 
-func GuideNew(getGuide Guide) Guide {
-	setNewGuide := getGuide
+func GuideNew(getGuideId int, getGuideTitle, getGuideDescription, getGuideUrl, getGuidePublished, getGuideUpdated, getGuideContent string) Guide {
+	setNewGuide := Guide{
+		Id:          getGuideId,
+		Title:       getGuideTitle,
+		Description: getGuideDescription,
+		Url:         getGuideUrl,
+		Published:   getGuidePublished,
+		Updated:     getGuideUpdated,
+		Content:     getGuideContent,
+	}
 	return setNewGuide
+}
+
+func GuideAddNewToDB(getGuide Guide) error {
+	db := database.DatabaseConnection()
+	defer db.Close()
+
+	query, err := db.Query(
+		"INSERT INTO guides (title, description, url, published, updated, content) VALUES (?, ?, ?, ?, ?, ?)",
+		getGuide.Title, getGuide.Description, getGuide.Url, getGuide.Published, getGuide.Updated, getGuide.Content,
+	)
+	if err != nil {
+		return fmt.Errorf("error adding a new guide: %w", err)
+	}
+	defer query.Close()
+
+	return nil
+}
+
+func GuideShowGuides() ([]Guide, error) {
+	db := database.DatabaseConnection()
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM guides")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var allGuides []Guide
+	for rows.Next() {
+		var guideId int
+		var guideTitle string
+		var guideDescription string
+		var guideUrl string
+		var guidePublished string
+		var guideUpdated string
+		var guideContent string
+		err = rows.Scan(&guideId, &guideTitle, &guideDescription, &guideUrl, &guidePublished, &guideUpdated, &guideContent)
+		if err != nil {
+			return nil, err
+		}
+
+		guideDetails := GuideNew(guideId, guideTitle, guideDescription, guideUrl, guidePublished, guideUpdated, guideContent)
+		allGuides = append(allGuides, guideDetails)
+	}
+
+	return allGuides, nil
 }
