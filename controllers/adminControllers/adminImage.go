fd50b86ddb505720fc4d80d12c675be893e2d847
MarcoValeri <marcovaleri@hotmail.it>
2024-07-02 15:37:29 +0100

    Add new logic that allows to upload images

--- controllers/adminControllers/adminImage.go
+++ controllers/adminControllers/adminImage.go
@@ -3,7 +3,10 @@ package admincontrollers
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func AdminUploadImage() {
@@ -15,6 +18,42 @@ func AdminUploadImage() {
 			fmt.Println("Error on session-authentication:", errSession)
 		}
 		if session.Values["user-admin-authentication"] == true {
+			// TODO: store image data into the db and upload image into its folder
+
+			// Form inputs
+			imageAddNew := r.FormValue("image-add-new")
+			imageFile, header, errImageFile := r.FormFile("image-file")
+
+			if len(imageAddNew) > 0 {
+				if errImageFile != nil {
+					fmt.Println("Error retrieving the image file:", errImageFile)
+					return
+				}
+				// defer imageFile.Close()
+
+				imagePath := filepath.Join("public", "images", header.Filename)
+				absImagePath, errImagePath := filepath.Abs(imagePath)
+				if errImagePath != nil {
+					fmt.Println("Error determing image path:", errImagePath)
+					return
+				}
+
+				dst, erDst := os.Create(absImagePath)
+				if erDst != nil {
+					fmt.Println("Error creating image file:", erDst)
+				}
+
+				_, errCopy := io.Copy(dst, imageFile)
+				if errCopy != nil {
+					fmt.Println("Error saving image file:", errCopy)
+					return
+				}
+
+				defer dst.Close()
+				defer imageFile.Close()
+
+				http.Redirect(w, r, "/admin/dashboard", http.StatusSeeOther)
+			}
 
 			tmpl.Execute(w, nil)
 		} else {
